e68ee107caae22a62a187132eb4acc7bb7b63ab9
Yeqi An <1393380109@qq.com>
2024-07-03 23:43:14 +0800

    feature:用户信息脱敏开发，优化Controller的返回结果

--- admin/src/main/java/com/project/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/project/shortlink/admin/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.project.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.project.shortlink.admin.common.convention.result.Result;
+import com.project.shortlink.admin.common.convention.result.Results;
+import com.project.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.project.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    /**
+     * 通过用户名查询用户的脱敏后的信息
+     */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result GetUserByUsername(@PathVariable String username){
-        Result result = userService.GetUserByUsername(username);
-        return result;
+        return Results.success(userService.GetUserByUsername(username));
+    }
+
+    /**
+     * 查询用户真实信息
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result GetActualUserByUsername(@PathVariable String username){
+        return Results.success(BeanUtil.toBean(userService.GetUserByUsername(username).getData(), UserActualRespDTO.class));
     }
 }
--- admin/src/main/java/com/project/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/project/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,16 +1,11 @@
 package com.project.shortlink.admin.dto.resp;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.project.shortlink.admin.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
-/**
- * 前端响应用户实体类
- */
 @Data
 @Accessors(chain = true)
-public class UserRespDTO {
+public class UserActualRespDTO {
     /**
      * 用户名
      */
@@ -24,12 +19,10 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
      * 邮箱
      */
     private String mail;
-
 }
