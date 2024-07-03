c442d555a521f9929f2b30927fc88847f1ebcf62
Yeqi An <1393380109@qq.com>
2024-07-03 21:52:01 +0800

    feature:开发短链接规约功能

---
+++ admin/src/main/java/com/project/shortlink/admin/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,11 @@
+package com.project.shortlink.admin.common.convention.errorcode;
+
+/**
+ * 异常码接口
+ */
+public interface IErrorCode {
+    //状态码
+    String code();
+    //异常信息
+    String message();
+}
