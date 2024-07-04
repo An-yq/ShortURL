5fa11dd8ecc74898a1e21fce26e05efcbb33de4d
Yeqi An <1393380109@qq.com>
2024-07-04 16:55:47 +0800

    feature:实现元对象处理器，实现注册和修改用户时自动更新创建和修改时间

--- admin/src/main/java/com/project/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/project/shortlink/admin/dao/entity/UserDO.java
@@ -1,7 +1,8 @@
 package com.project.shortlink.admin.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import jdk.jfr.DataAmount;
 import lombok.Data;
 
 import java.util.Date;
@@ -42,15 +43,18 @@ public class UserDO {
     /**
      * 创建时间
      */
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
      * 更新时间
      */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 
     /**
      * 删除标识 0:未删除 1:已删除
      */
+    @TableField(fill = FieldFill.INSERT)
     private Integer delFlag;
 }
