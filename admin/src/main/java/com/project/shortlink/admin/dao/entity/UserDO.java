86819421ca400a12c790fbce6e200cb10e9fb6c4
Yeqi An <1393380109@qq.com>
2024-07-05 21:25:55 +0800

    optimize:优化用户持久层实体类的相关基础字段的抽象，添加id字段

--- admin/src/main/java/com/project/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/project/shortlink/admin/dao/entity/UserDO.java
@@ -1,15 +1,16 @@
 package com.project.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.project.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.util.Date;
 
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
+    /**
+     * 用户id
+     */
+    private Long id;
     /**
      * 用户名
      */
@@ -40,21 +41,4 @@ public class UserDO {
      */
     private Long deletionTime;
 
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 更新时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0:未删除 1:已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 }
