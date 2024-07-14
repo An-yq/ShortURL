b58002bf317589857de188734a948c37787f72dd
Yeqi An <1393380109@qq.com>
2024-07-14 13:00:41 +0800

    feature:短链接项目基础组件初始化

---
+++ project/src/main/java/com/project/shortlink/project/common/convention/errorcode/IErrorCode.java
@@ -0,0 +1,11 @@
+package com.project.shortlink.project.common.convention.errorcode;
+
+/**
+ * 异常码接口
+ */
+public interface IErrorCode {
+    //状态码
+    String code();
+    //异常信息
+    String message();
+}
---
+++ project/src/main/java/com/project/shortlink/project/config/MyMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.project.shortlink.project.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 元对象处理器，用于自动填充创建时间和更新时间
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("插入填充创建时间，更新时间，删除标记...");
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("更新时间...");
+        this.strictUpdateFill(metaObject,"updateTime",Date.class, new Date());
+    }
+}
