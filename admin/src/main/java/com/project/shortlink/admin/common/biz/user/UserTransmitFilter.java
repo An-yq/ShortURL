d870c67684f8e0d1b7a2d14405edc3109244db1b
Yeqi An <1393380109@qq.com>
2024-07-15 21:04:13 +0800

    feature:短链接过滤器返回用户友好提示信息

--- admin/src/main/java/com/project/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/project/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,14 +1,24 @@
 package com.project.shortlink.admin.common.biz.user;
 
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import com.project.shortlink.admin.common.convention.exception.ClientException;
+import com.project.shortlink.admin.common.convention.result.Results;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 import java.util.Objects;
+
+import static com.project.shortlink.admin.common.enums.UserErrorCode.USER_TOKEN_FAIL;
 
 /**
  * 用户传输过滤器
@@ -16,15 +26,41 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/admin/v1/actual/user/has-username"
+    );
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
         String requestURI = httpServletRequest.getRequestURI();
-        if (!Objects.equals(requestURI,"/api/short-link/v1/user/login")) {
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+        if (!IGNORE_URI.contains(requestURI)) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    try {
+                        returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        throw new ClientException(USER_TOKEN_FAIL);
+                    }
+                } catch (Exception ex) {
+                    try {
+                        returnJson((HttpServletResponse) servletResponse,  JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    return;
+                }
                 UserInfoDTO userInfoDTO = JSONUtil.toBean(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
@@ -33,6 +69,21 @@ public class UserTransmitFilter implements Filter {
             filterChain.doFilter(servletRequest,servletResponse);
         }finally {
             UserContext.removeUser();
+        }
+    }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+
+        } catch (IOException e) {
+        } finally {
+            if (writer != null)
+                writer.close();
         }
     }
 }
--- admin/src/main/java/com/project/shortlink/admin/common/enums/UserErrorCode.java
+++ admin/src/main/java/com/project/shortlink/admin/common/enums/UserErrorCode.java
@@ -4,6 +4,8 @@ import com.project.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCode implements IErrorCode {
 
+    USER_TOKEN_FAIL("A000200","用户Token验证失败"),
+
     USER_NULL("B000200","用户不存在"),
     USER_NAME_EXIST("B000201","用户名已存在"),
     USER_EXIST("B000202","用户已存在"),
