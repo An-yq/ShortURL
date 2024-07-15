14f1106adf42575f6444e9eabf7df4613e47ea58
Yeqi An <1393380109@qq.com>
2024-07-15 20:39:30 +0800

    fix:修改admin中remote模块中的reqDTO

--- admin/src/main/java/com/project/shortlink/admin/remote/dto/ShortLinkPageReqDTO.java
+++ admin/src/main/java/com/project/shortlink/admin/remote/dto/ShortLinkPageReqDTO.java
@@ -1,11 +1,11 @@
 package com.project.shortlink.admin.remote.dto;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.project.shortlink.project.dao.entity.ShortLinkDO;
+
 import lombok.Data;
 
 @Data
-public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+public class ShortLinkPageReqDTO extends Page {
     /**
      * 分组标识
      */
