557fe0f2f23ed613adea2068f4c8e5101a7348c1
Yeqi An <1393380109@qq.com>
2024-07-15 23:46:09 +0800

    optimize:优化短链接后管系统查询短链接分组集合返回结果，新增分组内短链接数量属性

--- admin/src/main/java/com/project/shortlink/admin/remote/service/ShortLinkRemoteService.java
+++ admin/src/main/java/com/project/shortlink/admin/remote/service/ShortLinkRemoteService.java
@@ -8,9 +8,11 @@ import com.project.shortlink.admin.common.convention.result.Result;
 import com.project.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.project.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.project.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.project.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.project.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -43,6 +45,14 @@ public interface ShortLinkRemoteService {
         requestMap.put("size", requestParam.getSize());
         String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/page", requestMap);
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+    default Result<List<ShortLinkGroupCountQueryRespDTO>> listGroupShortLinkCount(List<String> requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("requestParam",requestParam);
+        String resultPageStr  = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", requestMap);
+        return JSON.parseObject(resultPageStr,new TypeReference<>() {
         });
     }
 }
--- admin/src/main/java/com/project/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/project/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -7,20 +7,27 @@ import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.project.shortlink.admin.common.biz.user.UserContext;
+import com.project.shortlink.admin.common.convention.result.Result;
 import com.project.shortlink.admin.dao.entity.GroupDO;
 import com.project.shortlink.admin.dao.mapper.GroupMapper;
 import com.project.shortlink.admin.dto.req.ShortLinkGroupSortReqDTO;
 import com.project.shortlink.admin.dto.req.ShortLinkGroupUpdateReqDTO;
 import com.project.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
+import com.project.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
+import com.project.shortlink.admin.remote.service.ShortLinkRemoteService;
 import com.project.shortlink.admin.service.GroupService;
 import com.project.shortlink.admin.tooklit.RandomStringGenerator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implements GroupService {
 
+    private ShortLinkRemoteService shortLinkRemoteService= new ShortLinkRemoteService(){
+    };
     @Override
     public void save(String groupName) {
         while(true){
@@ -57,8 +64,20 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
                 .eq(GroupDO::getDelFlag, 0)
                 .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
         List<GroupDO> groupDOS = baseMapper.selectList(queryWrapper);
-        List<ShortLinkGroupRespDTO> respDTOS = BeanUtil.copyToList(groupDOS, ShortLinkGroupRespDTO.class);
-        return respDTOS;
+        //查询分组时要返回分组内的短链接数量 --- 这里使用了stream流实现了在一个类list里面抽取属性list
+        Result<List<ShortLinkGroupCountQueryRespDTO>> listResult = shortLinkRemoteService.listGroupShortLinkCount(groupDOS.stream().map(GroupDO::getGid).toList());
+        //将groupDOS中的属性映射到shortLinkGroupRespDTOList中
+        List<ShortLinkGroupRespDTO> shortLinkGroupRespDTOList = BeanUtil.copyToList(groupDOS, ShortLinkGroupRespDTO.class);
+        //将listResult中的shortLinkCount抽取出来放到每一个GroupDO中
+        shortLinkGroupRespDTOList.forEach(each -> {
+            //依旧使用stream流，过滤出gid相同的，获取到这个ShortLinkGroupCountQueryRespDTO对象
+            Optional<ShortLinkGroupCountQueryRespDTO> first = listResult.getData()
+                    .stream()
+                    .filter(item -> Objects.equals(item.getGid(), each.getGid()))
+                    .findFirst();
+            first.ifPresent(item -> each.setShortLinkCount(item.getShortLinkCount()));
+        });
+        return shortLinkGroupRespDTOList;
     }
 
     @Override
