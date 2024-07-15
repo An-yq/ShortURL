f3e99bd1e1da2fda9a8a4daea6b248f46d5d9510
Yeqi An <1393380109@qq.com>
2024-07-15 20:53:18 +0800

    feature:对短链接分组数据库表水平分表

--- admin/src/test/java/com/project/shortlink/admin/UserTableShardingTest.java
+++ admin/src/test/java/com/project/shortlink/admin/UserTableShardingTest.java
@@ -2,29 +2,20 @@ package com.project.shortlink.admin;
 
 public class UserTableShardingTest {
     public static void main(String[] args) {
-        String SQL = "create table if not exists link.t_link_%d\n" +
+        String SQL = "create table if not exists link.t_group_%d\n" +
                 "(\n" +
-                "    id              bigint auto_increment comment '短链接id'\n" +
+                "    id          bigint auto_increment comment 'ID'\n" +
                 "        primary key,\n" +
-                "    domain          varchar(128) charset utf8mb4                  null comment '域名',\n" +
-                "    short_uri       varchar(8)                                    null comment '短链接',\n" +
-                "    full_short_url  varchar(128) charset utf8mb4                  null comment '完整短链接',\n" +
-                "    origin_url      varchar(1024) charset utf8mb4                 null comment '原始链接',\n" +
-                "    click_num       int                         default 0         null comment '点击次数',\n" +
-                "    gid             varchar(32) charset utf8mb4 default 'default' null comment '分组id',\n" +
-                "    enable_status   tinyint(1)                                    null comment '启用状态 0：启用 1：未启用',\n" +
-                "    create_type     tinyint(1)                                    null comment ' 创建类型 0：自定义 1：控制台',\n" +
-                "    valid_date_type tinyint(1)                                    null comment '有效期类型 0：永久有效 1：自定义有效期',\n" +
-                "    valid_date      datetime                                      null comment '有效日期',\n" +
-                "    `describe`      varchar(1021) charset utf8mb4                 null comment '描述',\n" +
-                "    create_time     datetime                                      null comment '创建时间',\n" +
-                "    update_time     datetime                                      null comment '更新时间',\n" +
-                "    del_flag        tinyint(1)                                    null comment '删除标识 0：未删除 1：已删除',\n" +
-                "    favicon         varchar(256)                                  null,\n" +
-                "    constraint uni_full_short_url\n" +
-                "        unique (full_short_url)\n" +
-                ")\n" +
-                "    collate = utf8mb4_bin;";
+                "    gid         varchar(32)  null comment '分组标识',\n" +
+                "    name        varchar(64)  null comment '分组名称',\n" +
+                "    username    varchar(256) null comment '创建分组用户名',\n" +
+                "    sort_order  int          not null,\n" +
+                "    create_time datetime     null comment '创建时间',\n" +
+                "    update_time datetime     null comment '更新时间',\n" +
+                "    del_flag    tinyint(1)   null comment '删除标识',\n" +
+                "    constraint uni_gid_username\n" +
+                "        unique (gid, username) comment 'gid和username唯一索引'\n" +
+                ");";
         for (int i = 0; i < 16; i++) {
             System.out.printf(SQL,i);
         }
