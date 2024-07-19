e8a12b02f65d645d7a136356cfb7a23bc86bc37b
Yeqi An <1393380109@qq.com>
2024-07-19 22:30:45 +0800

    feature: 实现短链接获取原始链接标题功能

--- admin/src/main/java/com/project/shortlink/admin/remote/service/ShortLinkRemoteService.java
+++ admin/src/main/java/com/project/shortlink/admin/remote/service/ShortLinkRemoteService.java
@@ -10,6 +10,7 @@ import com.project.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.project.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.project.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.project.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
@@ -53,6 +54,18 @@ public interface ShortLinkRemoteService {
         requestMap.put("requestParam",requestParam);
         String resultPageStr  = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultPageStr,new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 根据 URL 获取标题
+     *
+     * @param url 目标网站地址
+     * @return 网站标题
+     */
+    default Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        String resultStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/title?url=" + url);
+        return JSON.parseObject(resultStr, new TypeReference<>() {
         });
     }
 }
---
+++ project/src/main/java/com/project/shortlink/project/controller/UrlTitleController.java
@@ -0,0 +1,24 @@
+package com.project.shortlink.project.controller;
+
+import com.project.shortlink.project.common.convention.result.Result;
+import com.project.shortlink.project.common.convention.result.Results;
+import com.project.shortlink.project.service.UrlTitleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UrlTitleController {
+
+    private final UrlTitleService urlTitleService;
+
+    /**
+     * 根据 URL 获取对应网站的标题
+     */
+    @GetMapping("/api/short-link/v1/title")
+    public Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        return Results.success(urlTitleService.getTitleByUrl(url));
+    }
+}
