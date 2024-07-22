38ae77314c1ddef8063248cde51c2b85d06f470e
Yeqi An <1393380109@qq.com>
2024-07-22 15:12:21 +0800

    feature: 根据短链接获取指定日期内PV、UV、UIP数据

--- project/src/main/java/com/project/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
+++ project/src/main/java/com/project/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -2,6 +2,7 @@ package com.project.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.project.shortlink.project.dao.entity.LinkAccessLogsDO;
+import com.project.shortlink.project.dao.entity.LinkAccessStatsDO;
 import com.project.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -83,4 +84,20 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
             @Param("endDate") String endDate,
             @Param("userAccessLogsList") List<String> userAccessLogsList
     );
+    /**
+     * 根据短链接获取指定日期内PV、UV、UIP数据
+     */
+    @Select("SELECT " +
+            "    COUNT(user) AS pv, " +
+            "    COUNT(DISTINCT user) AS uv, " +
+            "    COUNT(DISTINCT ip) AS uip " +
+            "FROM " +
+            "    t_link_access_logs " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND create_time BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    full_short_url, gid;")
+    LinkAccessStatsDO findPvUvUidStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
 }
